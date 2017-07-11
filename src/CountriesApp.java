c7cc2581d07b286369bc0684113d901b1f5e142d
thesuz88 <stevensuzio@gmail.com>
2017-07-11 15:12:00 -0400

    added methods to class and validation

--- src/CountriesApp.java
+++ src/CountriesApp.java
@@ -7,68 +7,39 @@ import java.util.Scanner;
 public class CountriesApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        CountriesTextFIle countryFile = new CountriesTextFIle();
         int menuNum = 0;
 
         System.out.println("Welcome to the Countries Maintenance Application!");
-        System.out.println("1 - See the list of countries\n2 - Add a country\n3 - Exit");
         do {
             System.out.println("\nEnter menu number");
+            System.out.println("1 - See the list of countries\n2 - Add a country\n3 - Exit");
 
             menuNum = scan.nextInt();
 
-            while (menuNum != 1 && menuNum != 2 && menuNum != 3) {
-                System.out.println("Invalid menu choice.");
-                System.out.println("1 - See the list of countries\n2 - Add a country\n3 - Exit");
-                System.out.println("\nEnter menu number");
-                menuNum = scan.nextInt();
-            }
+            menuNum = validateMenuNum(scan, menuNum);
 
             if (menuNum == 1) {
-                //show list of countries (filereader)
-                System.out.println("Current List of Countries:");
-                File countryFile = new File("countries.txt");
-                try {
-                    FileReader reader = new FileReader(countryFile);
-                    BufferedReader buff = new BufferedReader(reader);
-                    String line;
-                    while ((line = buff.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                countryFile.readFromFile();
 
             } else if (menuNum == 2) {
                 //add a country (filewriter)
-                System.out.println("Enter Country: ");
-                scan.nextLine();
-                String countryAdd = scan.nextLine();
-                try {
-                    FileWriter newCountry = new FileWriter("countries.txt", true);
-                    newCountry.write(countryAdd + "\n");
-                    newCountry.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
+                countryFile.writeToFile();
             }
-
-
-//        try {
-//            FileWriter countries = new FileWriter("countries.txt", true);
-//
-//        }
-//        catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-        }while(menuNum != 3);
+        } while (menuNum != 3);
 
         System.out.println("Happy travels!");
     }
+
+    private static int validateMenuNum(Scanner scan, int menuNum) {
+        while (menuNum != 1 && menuNum != 2 && menuNum != 3) {
+            System.out.println("Invalid menu choice.");
+            System.out.println("1 - See the list of countries\n2 - Add a country\n3 - Exit");
+            System.out.println("\nEnter menu number");
+            menuNum = scan.nextInt();
+        }
+        return menuNum;
+    }
+
 
 }
--- src/CountriesTextFIle.java
+++ src/CountriesTextFIle.java
@@ -1,15 +1,40 @@
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 /**
  * Created by Steve on 7/11/2017.
  */
 public class CountriesTextFIle {
+    Scanner scan = new Scanner(System.in);
 
+    public void readFromFile() {
+        //show list of countries (filereader)
+        System.out.println("Current List of Countries:");
+        File countryFile = new File("countries.txt");
+        try {
+            FileReader reader = new FileReader(countryFile);
+            BufferedReader buff = new BufferedReader(reader);
+            String line;
+            while ((line = buff.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-
-
-
-
-
+    public void writeToFile() {
+        //write list of countries (FileWriter)
+        System.out.println("Enter Country: ");
+        String countryAdd = scan.nextLine();
+        try {
+            FileWriter newCountry = new FileWriter("countries.txt", true);
+            newCountry.write(countryAdd + "\n");
+            newCountry.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
